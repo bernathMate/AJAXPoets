7a06288342539bcb7e55e23f885c8e7c364a980f
Bernáth Máté <bernath.mate.new@gmail.com>
2018-04-30 12:41:23 +0200

    This class has two method: getPoemsByPoetId, this method will return a list of Poems object, based on Poet's id, getPoemByPoetIdAndPoemId, this method will return a Poem object, based on Poet's id, and Poem's id

---
+++ src/main/java/com/codecool/web/service/PoemService.java
@@ -0,0 +1,14 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Poem;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface PoemService {
+
+    List<Poem> getPoemsByPoetId(int poetId) throws SQLException, ServiceException;
+
+    Poem getPoemByPoetIdAndPoemId(int poetId, int poemId) throws SQLException, ServiceException;
+}
