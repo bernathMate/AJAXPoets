62970d9c31282b6645abfe6a793a85fd5a6b91ea
Bernáth Máté <bernath.mate.new@gmail.com>
2018-05-01 16:32:05 +0200

    doPost method is implemented, this method will send the number of the occurence from the given poem's content to JS.

--- src/main/java/com/codecool/web/servlet/PoemServlet.java
+++ src/main/java/com/codecool/web/servlet/PoemServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -24,8 +25,10 @@ public class PoemServlet extends AbstractServlet {
         try (Connection connection = getConnection(req.getServletContext())) {
             PoemDao poemDao = new DatabasePoemDao(connection);
             PoemService poemService = new SimplePoemService(poemDao);
+            HttpSession session = req.getSession();
 
             String poemId = req.getParameter("id");
+            session.setAttribute("poemId", poemId);
             int poetId = ((Poet) req.getSession().getAttribute("poet")).getId();
             Poem poem = poemService.getPoemByPoetIdAndPoemId(poetId, Integer.parseInt(poemId));
 
@@ -37,4 +40,23 @@ public class PoemServlet extends AbstractServlet {
         }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            PoemDao poemDao = new DatabasePoemDao(connection);
+            PoemService poemService = new SimplePoemService(poemDao);
+
+            String poemId = req.getSession().getAttribute("poemId").toString();
+            int poetId = ((Poet) req.getSession().getAttribute("poet")).getId();
+            String substring = req.getParameter("substring");
+
+            int occurenceNumber = poemService.getSubstringOccurenceByPoetIdAndPoemId(poetId, Integer.parseInt(poemId), substring);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, occurenceNumber);
+        } catch (ServiceException e) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        }
+    }
 }
