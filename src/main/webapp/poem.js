d8df4cd00410e9f5bc37bcc93ebffe6d72546096
Bernáth Máté <bernath.mate.new@gmail.com>
2018-05-01 16:36:26 +0200

    onSearchClicked, onSearchResponse, searchSubstring methods are implemented, these methods will display the occurence number

--- src/main/webapp/poem.js
+++ src/main/webapp/poem.js
@@ -1,3 +1,36 @@
+function searchSubstring(occurenceNumber) {
+    const count = document.getElementById('occurence-number');
+
+    count.textContent = occurenceNumber;
+}
+
+function onSearchResponse() {
+    clearMessages();
+    if (this.status === OK) {
+        searchSubstring(JSON.parse(this.responseText));
+    } else {
+        const errorDivEl = document.getElementById("error");
+        onOtherResponse(errorDivEl, this);
+    }
+}
+
+function onSearchClicked() {
+    const occurenceFormEl = document.forms['occurence-form'];
+
+    const substringInputEl = occurenceFormEl.querySelector('input[name="substring"]');
+
+    const substring = substringInputEl.value;
+
+    const params = new URLSearchParams();
+    params.append('substring', substring);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onSearchResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('POST', 'protected/poem');
+    xhr.send(params);
+}
+
 function onPoemLoad(poem) {
     const poemContentSpanEl = document.getElementById('content');
 
@@ -7,9 +40,9 @@ function onPoemLoad(poem) {
 function onPoemResponse() {
     if (this.status === OK) {
         clearMessages();
-        showContents(['profile-content', 'poems-content', 'poem-content','logout-content']);
+        showContents(['profile-content', 'poems-content', 'poem-content', 'logout-content']);
         onPoemLoad(JSON.parse(this.responseText));
     } else {
-        onOtherResponse(shopsContentDivEl, this);
+        onOtherResponse(poemContentDivEl, this);
     }
 }
